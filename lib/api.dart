2fc03b5673e7c381e76bb2d03dc41bf11d03062a
Felix Dernhagen <felix@dernhagen.se>
2021-11-25 12:40:29 +0100

    Uppdaterat todo_list_state efter http metoder

--- lib/api.dart
+++ lib/api.dart
@@ -10,8 +10,9 @@ const API_URL =
 class Api {
 //lägger till en todo
   static Future<List<ToDoItem>> addTask(ToDoItem task) async {
-    Map<String, dynamic> json = ToDoItem.toJson(task);
-    var bodyString = jsonEncode(json);
+    var bodyString = jsonEncode({
+      'title': task.toDoText,
+    });
     var response = await http.post(
       Uri.parse('$API_URL/todos?key=$API_KEY'),
       body: bodyString,
@@ -28,6 +29,17 @@ class Api {
   static Future deleteTask(String taskId) async {
     var response = await http
         .delete(Uri.parse('$API_URL/todos/$taskId?key=$API_KEY&_confirm=true'));
+    var bodyString = response.body;
+    var list = jsonDecode(bodyString);
+
+    return list.map<ToDoItem>((data) {
+      return ToDoItem.fromJson(data);
+    }).toList();
+  }
+
+  static Future changeTask(String taskId) async {
+    var response = await http
+        .put(Uri.parse('$API_URL/todos/$taskId?key=$API_KEY&_confirm=true'));
     var bodyString = response.body;
     var list = jsonDecode(bodyString);
 
